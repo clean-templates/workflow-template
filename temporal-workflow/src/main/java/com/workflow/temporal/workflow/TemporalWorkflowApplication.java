21cb849de32f24bcedb6c71d4a0c1e794159ae5f
RolandSall <roland.salloum00@outlook.com>
2023-02-26 14:46:44 +0200

    feat: initial temporal workflow module

---
+++ temporal-workflow/src/main/java/com/workflow/temporal/workflow/TemporalWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.workflow.temporal.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemporalWorkflowApplication.class, args);
+    }
+
+}
---
+++ temporal-workflow/src/test/java/com/workflow/temporal/workflow/TemporalWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflow.temporal.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalWorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
