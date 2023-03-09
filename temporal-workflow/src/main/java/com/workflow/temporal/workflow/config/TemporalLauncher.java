423247fdbfad71f8accf7d13ec6a0cc4b0fd53ad
RolandSall <roland.salloum00@outlook.com>
2023-03-09 09:27:16 +0200

    feat: experimented with namespace

--- temporal-workflow/src/main/java/com/workflow/temporal/workflow/config/TemporalLauncher.java
+++ temporal-workflow/src/main/java/com/workflow/temporal/workflow/config/TemporalLauncher.java
@@ -1,34 +1,58 @@
 package com.workflow.temporal.workflow.config;
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowClientOptions;
+import io.temporal.common.converter.DefaultDataConverter;
+import io.temporal.common.converter.JacksonJsonPayloadConverter;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import io.temporal.serviceclient.WorkflowServiceStubsOptions;
 import io.temporal.worker.WorkerFactory;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Data
-@AllArgsConstructor
 @Configuration
 public class TemporalLauncher {
+
+    @Value("${temporal.url}")
+    private String url;
+
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public WorkflowClientOptions workflowClientOptions(ObjectMapper objectMapper) {
+        return WorkflowClientOptions.newBuilder()
+                .setNamespace("test-namespace")
+                .setDataConverter(DefaultDataConverter
+                        .newDefaultInstance()
+                        .withPayloadConverterOverrides(new JacksonJsonPayloadConverter(objectMapper))              )
+                .build();
+    }
 
     @Bean
     public WorkflowServiceStubs workflowServiceStubs() {
         return WorkflowServiceStubs.newServiceStubs(
                 WorkflowServiceStubsOptions.newBuilder()
-                        .setTarget("localhost:7233")
+                        .setTarget(url)
                         .build());
 
 
     }
 
     @Bean
-    public WorkflowClient workflowClient(WorkflowServiceStubs workflowServiceStubs) {
-        return WorkflowClient.newInstance(workflowServiceStubs);
+    public WorkflowClient workflowClient(WorkflowServiceStubs temporalWorkflowService, WorkflowClientOptions workflowClientOptions) {
+        return WorkflowClient.newInstance(temporalWorkflowService, workflowClientOptions);
     }
+
+
 
     @Bean
     public WorkerFactory workerFactory(WorkflowClient client) {
